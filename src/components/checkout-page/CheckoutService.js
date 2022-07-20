9410dbfabbb3066bcb0feb80da1fcd9bac1e3bae
Hayes McCardell II <hmccardell@catalyte.io>
2022-07-20 13:22:48 -0500

    initial project commit

---
+++ src/components/checkout-page/CheckoutService.js
@@ -0,0 +1,25 @@
+import HttpHelper from '../../utils/HttpHelper';
+import Constants from '../../utils/constants';
+
+/**
+ *
+ * @name makePayment
+ * @description sends a purchase request
+ * @param {*} cartContents items to purchase
+ * @returns payment confirmation response
+ */
+const makePurchase = async (products, deliveryAddress, billingAddress, creditCard) => {
+  await HttpHelper(Constants.PURCHASE_ENDPOINT, 'POST', {
+    products,
+    deliveryAddress,
+    billingAddress,
+    creditCard
+  })
+    .then((response) => response.json())
+    .catch(() => {
+      /* eslint-disable no-console */
+      console.log('Failed to purchase');
+      /* eslint-enable no-console */
+    });
+};
+export default makePurchase;
