ac0ec63d980d71df865cee9a16975b5e465385b5
Chandler Davis <cdavis@catalyte.io>
2023-04-25 17:01:33 +0000

    JH-71 Toast Severity Messages

--- src/components/toast/Toast.js
+++ src/components/toast/Toast.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Snackbar from '@material-ui/core/Snackbar';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
+import Alert from '@material-ui/lab/Alert';
 
 /**
  * @name Toast - Reusable toast that will pop up in the top left corner,
  * disappears automatically after 8 seconds.
  * @param {message} String, what the toast will say
  * @param {open} Boolean - state to be set. False = toast closed (default), True = toast open
- * @param {handleClose} Function set open parameter to false.
+ * @param {handleClose} Function that sets open parameter to false.
+ * @param {severity} String from the following options to change the message type:
+ * [success(green), warning(yellow), info(blue), error(red)]
+ * Info is the default if not defined in the component.
  */
-export default function Toast({ message, open, handleClose }) {
+export default function Toast({
+  message, open, handleClose, severity = 'info'
+}) {
   return (
     <>
       <Snackbar
@@ -21,15 +25,11 @@ export default function Toast({ message, open, handleClose }) {
         open={open}
         autoHideDuration={8000}
         onClose={handleClose}
-        message={message}
-        action={(
-          <>
-            <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
-              <CloseIcon fontSize="small" />
-            </IconButton>
-          </>
-        )}
-      />
+      >
+        <Alert onClose={handleClose} severity={severity}>
+          {message}
+        </Alert>
+      </Snackbar>
     </>
   );
 }
