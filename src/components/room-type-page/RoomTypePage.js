f465a7e1c2575daf573349a2e042af7ce4c3dbc5
Chandler Davis <cdavis@catalyte.io>
2023-06-23 17:02:35 -0400

    room type page styling updated

--- src/components/room-type-page/RoomTypePage.js
+++ src/components/room-type-page/RoomTypePage.js
@@ -24,22 +24,27 @@ const RoomTypePage = () => {
 
   return (
     <article>
-      <h1>Room Types</h1>
-      <section>
-        <h2>Create New</h2>
-        <div className={styles.buttonSection} />
-        <Button
-          style={{ backgroundColor: '#395aa1', color: 'white', borderRadius: 20 }}
-          disabled={false}
-          size="small"
-          variant="contained"
-          startIcon={<Add />}
-          onClick={() => history.push('room-types/create')}
-        >
-          Room Type
-        </Button>
-      </section>
+      <div className={styles.roomTypeHeader}>
+
+        <section>
+          <h2>Create New</h2>
+          <div className={styles.buttonSection}>
+            <Button
+              style={{ backgroundColor: '#395aa1', color: 'white', borderRadius: 20 }}
+              disabled={false}
+              size="small"
+              variant="contained"
+              startIcon={<Add />}
+              onClick={() => history.push('room-types/create')}
+            >
+              Room Type
+            </Button>
+          </div>
+        </section>
+      </div>
+
       {apiError && <AppAlert severity="error" title="Error" message={Constants.API_ERROR} />}
+      <h1 className={styles.title}>Room Types</h1>
       <section className={styles.app}>
         {roomTypes.map((roomType) => (
           <div key={roomType.id}>
